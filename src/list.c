a9fc88aa905d95fa2d5a43ce84c8fe096bf47374
Arthur Simon <rthrsmn@gmail.com>
2022-03-02 15:18:44 +0100

    add list file and parting from home

---
+++ src/list.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 22:36:59 by asimon            #+#    #+#             */
+/*   Updated: 2022/02/27 22:37:01 by asimon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../includes/minishell.h"
+
+t_cmd	*ft_add_list(t_cmd *start)
+{
+	t_cmd	*buff;
+	t_cmd	*new;
+
+	new = malloc(sizeof(t_cmd));
+	if (new == NULL)
+		return (NULL);
+	if (start != NULL)
+	{
+		buff = start->next;
+		while (buff != NULL)
+		{
+			start = start->next;
+			buff = start->next;
+		}
+		start->next = new;
+	}
+	new->next = NULL;
+	return (new);
+}
+
+int		main(int argc, char **argv, char **penv)
+{
+	(void)argc;
+	(void)argv;
+	t_cmd	*start;
+	t_cmd	*cmd;
+	int		i = 0;
+
+	start = ft_add_list(NULL);
+	cmd = start;
+	while (i < 4)
+	{
+		cmd = ft_add_list(cmd);
+		i++;
+	}
+	i = 0;
+	while (start != NULL)
+	{
+		printf("Creation de list | %p\n", start);
+		start = start->next;
+	}
+	return (0);
+}
---
+++ src/parting.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parting.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 14:53:04 by asimon            #+#    #+#             */
+/*   Updated: 2022/02/27 14:53:06 by asimon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../includes/minishell.h"
+
+t_cmd		*parting(int argc, char **argv, char **penv)
+{
+	int		arg_i;
+	t_cmd	*cmds;
+
+	argc_i = 0;
+	cmds = malloc(sizeof(t_cmd) * 1);
+	if (cmds == NULL)
+		return (NULL);
+	whil/e (arg_i < argc)
+	{
+		
+		arg_i++;
+	}
+}
