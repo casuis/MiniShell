a76631f3e1bf7c9ea4c397078e3e070195436212
Arthur SIMON <asimon@e3r4p4.clusters.42paris.fr>
2022-04-05 16:42:37 +0200

    one last file

--- src/exec/get_env_tab.c
+++ src/exec/get_env_tab.c
@@ -10,4 +10,63 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./include/minishell_exec.h"
 
+void	ft_putstr_in(char *ret, char *s1)
+{
+	int		i;
+
+	i = 0;
+	while (s1[i])
+	{
+		*ret = s1[i];
+		i++;
+		ret++;
+	}
+	*ret = '\0';
+}
+
+char	*ft_strjoinc(char *s1, char *s2, char c)
+{
+	int		i;
+	char	*ret;
+
+	i = 0;
+	ret = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 2));
+	if (s1)
+		ft_putstr_in(ret, s1);
+	if (c)
+	{
+		i = ft_strlen(ret);
+		ret[i] = c;
+		i++;
+	}
+	if (s2)
+		ft_putstr_in(&ret[i], s2);
+	i = ft_strlen(ret);
+	return (ret);
+}
+
+char	**get_env_tab()
+{
+	t_env	buff;
+	char	**ret;
+	int		i;
+
+	i = 0;
+	buff = shell.env;
+	while (buff->next != NULL)
+	{
+		i++;
+		buff = buff->next;
+	}
+	ret = malloc(sizeof(char *) * (i + 1);
+	buff = shell.env;
+	i = 0;
+	while (buff != NULL)
+	{
+		ret[i] = ft_strjoinc(buff->key, buff->value, '=');
+		i++;
+		buff = buff->next;
+	}
+}
